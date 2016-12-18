109bec26e95a3339adcdbb8a36513f27d2d13b06
killerapp7 <killerapp7@gmail.com>
2016-12-18 21:41:56 +0530

    in progress

--- app/src/main/java/com/example/arun/bizquiz/MainActivity.java
+++ app/src/main/java/com/example/arun/bizquiz/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.arun.bizquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +15,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void submitQuiz() {
+    public void submitQuiz(View v) {
 
+        for (int i = 1; i <= 1; i++) {
+            CheckAnswer(i);
+        }
+        Toast.makeText(getApplicationContext(), "Awesome", Toast.LENGTH_SHORT).show();
     }
 
+    public void CheckAnswer(int questionNumber) {
+        int rightOrWrongIconId = getResources().getIdentifier("rightOrWrongIcon" + questionNumber, "id", getPackageName());
+        int rightOrWrongTextId = getResources().getIdentifier("rightOrWrongText" + questionNumber, "id", getPackageName());
+        ImageView rightOrWrongIcon = (ImageView) findViewById(rightOrWrongIconId);
+        rightOrWrongIcon.setImageResource(R.drawable.wrong);
+        TextView rightOrWrongText = (TextView) findViewById(rightOrWrongTextId);
+        rightOrWrongText.setText("You are correct!");
+    }
 }
